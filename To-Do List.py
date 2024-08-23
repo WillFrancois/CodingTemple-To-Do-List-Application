3d0f7eba9c2a08357ccfa1e1362257c21599b8c9
WillFrancois <williamjcfrancois@gmail.com>
2024-08-23 16:31:25 -0400

    Added comments and removed repeating code blocks

--- To-Do List.py
+++ To-Do List.py
@@ -1,6 +1,7 @@
-#Initial variables for storing the To-Do list
+#Initial variables for storing and using the To-Do list
+menu_text = "Welcome to the To-Do List App!\n\nMenu:\n1. Add a task\n2. View tasks\n3. Mark a task as complete or incomplete\n4. Delete a task\n5. Quit\n\n"
 todo_list = []
-user_inp = input("Welcome to the To-Do List App!\n\nMenu:\n1. Add a task\n2. View tasks\n3. Mark a task as complete or incomplete\n4. Delete a task\n5. Quit\n\n")
+user_inp = input(menu_text)
 
 #Functions for modifying the todo_list variable
 def add_task(task):
@@ -11,6 +12,8 @@ def view_tasks():
     if len(todo_list) == 0:
         print("\nNo tasks!\n")
     else:
+        #Extra print statements are used for readability as newlines in the for loop can lead to messy output!
+        #These are also used in in the mark_complete and delete_task function for readability and spacing.
         print()
         for i in range(len(todo_list)):
             print(i+1, ". ", todo_list[i][0], " - Complete" if todo_list[i][1] else " - Incomplete", sep="")
@@ -24,9 +27,11 @@ def delete_task(task_number):
     todo_list.pop(task_number-1)
     print()
 
-def validate_task_number(number, function):
+#Validates user input for cases 3 and 4 in the main menu
+def validate_task_number(function):
     try:
-        function(number)
+        task_number = int(input("Please provide the task number you would like to delete: "))
+        function(task_number)
     except IndexError:
             print("\nTask value doesn't exist, please try again!\n")
     except ValueError:
@@ -35,6 +40,8 @@ def validate_task_number(number, function):
         print("\nSorry, something went wrong. Please try again!\n")
     else:
         print("Operation successful!\n")
+    #A finally block would go here if there was a case where code would run regardless of whether
+    #was a raised exception or not, but is not necessary for full functionality
 
 while user_inp != "5":
     match user_inp:
@@ -45,35 +52,19 @@ while user_inp != "5":
         case '2':
             view_tasks()
 
+        #Case 3 and 4 both utilize functions as arguments to allow both cases to
+        #use the same code without needing to have it repeated and improve readability
+
         case '3':
             view_tasks()
-            try:
-                task_number = int(input("Please provide the task number you would like to mark as complete or incomplete: "))
-                mark_complete(task_number)
-            except IndexError:
-                print("\nTask value doesn't exist, please try again!\n")
-            except ValueError:
-                print("\nInvalid input, please try again!\n")
-            except Exception:
-                print("\nSorry, something went wrong. Please try again!\n")
-            else:
-                print("Operation successful!\n")
+            validate_task_number(mark_complete)
 
         case '4':
             view_tasks()
-            try:
-                task_number = int(input("Please provide the task number you would like to delete: "))
-                delete_task(task_number)
-            except IndexError:
-                print("\nTask value doesn't exist, please try again!\n")
-            except ValueError:
-                print("\nInvalid input, please try again!\n")
-            except Exception:
-                print("\nSorry, something went wrong. Please try again!\n")
-            else:
-                print("Operation successful!\n")
+            validate_task_number(delete_task)
 
         case _:
-            print("Sorry, I was unable to process your input! Please try again.")
+            print("\nSorry, I was unable to process your input! Please try again.\n")
 
-    user_inp = input("Welcome to the To-Do List App!\n\nMenu:\n1. Add a task\n2. View tasks\n3. Mark a task as complete or incomplete\n4. Delete a task\n5. Quit\n\n")
+    #The loop begins again here!
+    user_inp = input(menu_text)
